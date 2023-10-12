b8982a048dab0503095343eeb7917ddad7592c2c
ToniNichev <toni.nichev@gmail.com>
2023-10-11 21:35:12 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# projects-the-night-walker
---
+++ projects-the-night-walker.ino
@@ -0,0 +1,146 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+
+struct Step {
+  float pos[8];
+  float speed;
+};
+
+
+Step steps[] = {
+  {
+    {
+      // front right leg
+      270 + 80,
+      270 + 80,
+      // front left leg
+      270 - 80,
+      270 - 80,
+
+      // rear right leg
+      270 + 120,
+      270,
+      // rear left leg
+      270 - 120,
+      270,        
+    },
+    1
+  },  
+};
+
+int tick = 0;
+int len = 7;
+int mode =  1;
+
+class Servo {
+  public:
+    int pin = 0;
+    float pos = 270;
+    int move = 1;
+
+    bool moveServo(int newPos, float speed) {
+      if(newPos > pos) { 
+        if(pos + speed > newPos)
+          pos = newPos;
+        else
+          pos = pos + speed;
+      }
+      else if(newPos < pos) {
+        if(pos - speed < newPos)
+          pos = newPos;
+        else
+          pos = pos - speed;
+      }
+      else if(newPos == pos) {
+        return true; // reached the new position
+      }
+      pwm.setPWM(pin, 0, pos);
+      return false;
+    }
+};
+
+class Legs {
+  private:
+    int mode = 0;
+    int dataTick = 0;
+    Servo servo[8];
+
+  public:
+    void init() {
+      for(int i = 0; i < 8; i ++) {
+        servo[i].pin = i;
+      }
+      pwm.setPWM(0, 0, 270);
+      pwm.setPWM(1, 0, 270);
+
+      pwm.setPWM(2, 0, 270);
+      pwm.setPWM(3, 0, 270);
+
+      pwm.setPWM(4, 0, 270);
+      pwm.setPWM(5, 0, 270);
+
+      pwm.setPWM(6, 0, 270);
+      pwm.setPWM(7, 0, 270);
+     mode = 1;
+    }
+
+    void moveLegs(Step* stepsData, int dataSize ) {
+      if(mode == 0)
+        return;
+      if(mode == 1) {
+        Serial.println("MODE 1");
+        Step step = stepsData[dataTick];
+
+        bool complete[8] = {false,false,false,false,false,false,false,false};
+        
+        for(int i = 0; i < 8; i++) {
+        
+          complete[i] = servo[i].moveServo(step.pos[i], step.speed);
+          if(complete[0] == true && complete[1] == true && complete[2] == true && complete[3] == true
+            && complete[4] == true && complete[5] == true && complete[6] == true && complete[7] == true)
+          {
+            Serial.println("###");
+            Serial.println(complete[0]);
+            mode = 2; // tick complete
+          }
+        }
+        
+      }
+      if(mode == 2) {
+        // move to the next step
+        Serial.println("MODE 2");
+        Serial.println(dataSize);
+        dataTick ++;
+        mode = 1;
+        if(dataTick == dataSize) {
+          mode = 0;
+        }
+      }
+    }
+};
+
+Legs legs;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Servo walker");
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates  
+  legs.init();
+  delay(2000);
+  Serial.println("Starting ...");
+}
+
+
+void loop() {
+  int size = sizeof(steps)/sizeof(steps[0]);
+  legs.moveLegs(steps, size);
+  delay(10);
+}
